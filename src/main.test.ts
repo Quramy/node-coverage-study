a528768d3ef1f8d0bc3aa8fbf495532087aabfc1
Yosuke Kurami <yosuke.kurami@gmail.com>
2020-01-08 05:40:09 +0900

    initial commit

---
+++ src/main.test.ts
@@ -0,0 +1,6 @@
+import { fnB } from './main';
+describe('fnB', () => {
+  it('should return 0', () => {
+    expect(fnB()).toBe(0);
+  });
+});
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+export function fnA() {
+  return 100;
+}
+
+export function fnB() {
+  return 0;
+}
+
+function main() {
+  console.log(fnA());
+}
+
+main();
