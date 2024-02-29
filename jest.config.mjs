1b4a981d3aa005cc1ee321aeaf7664e8b6155a13
Quramy <yosuke.kurami@gmail.com>
2024-02-29 22:47:40 +0900

    upgrade dependencies

---
+++ jest.config.mjs
@@ -0,0 +1,14 @@
+export default {
+  transform: {
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        diagnostics: false,
+      },
+    ],
+  },
+  testRegex: "(src/.*\\.test)\\.ts$",
+  testPathIgnorePatterns: ["/node_modules/", "\\.d\\.ts$", "lib/.*"],
+  collectCoverageFrom: ["src/**/*.ts"],
+  moduleFileExtensions: ["js", "ts", "json"],
+};
